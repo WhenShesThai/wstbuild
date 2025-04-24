fe72e26dc82658c5c2955487c70f1e5040188420
WhenShesThai <whenshesthai@gmail.com>
2025-04-24 13:47:34 +1200

    consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "When Shes Thai";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION = "Welcome to When Shes Thai!";
